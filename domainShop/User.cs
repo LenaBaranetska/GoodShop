bcd1b94b806f385f0db35a36a3f24ab45c3f9feb
Олександр Гузік <guzik@step.itstep.rv.ua>
2015-11-17 18:11:04 +0200

    Fixeb bug

--- domainShop/User.cs
+++ domainShop/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace domainShop
 {
-    class User
+    public class User
     {
         public string Id { get; set; }
         public string Name { get; set; }
